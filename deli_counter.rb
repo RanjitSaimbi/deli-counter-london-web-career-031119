cfc72daa044da8d86bd9fb35a1ff12ccd836f216
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 16:20:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,11 @@
 def line(queue)
   puts "The line is currently empty." if queue.length == 0 
 else 
+  newArray = []
+  queueeach_with_index { |name, index|
+  newArray << "#{index+1}. #{name} "
+  }
+  puts "The line is currently: "
+end 
   
 end 
