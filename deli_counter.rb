e35676f497e8bbdbd429fb9657d3d66b3f357eec
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 16:21:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 def line(queue)
   puts "The line is currently empty." if queue.length == 0 
-else 
-  newArray = []
-  queueeach_with_index { |name, index|
-  newArray << "#{index+1}. #{name} "
-  }
-  puts "The line is currently: #{newArray}"
-end 
-  
+  else 
+    newArray = []
+    queueeach_with_index { |name, index|
+    newArray << "#{index+1}. #{name} "
+    }
+    puts "The line is currently: #{newArray}"
+  end 
 end 
